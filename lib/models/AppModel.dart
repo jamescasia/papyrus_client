0d9f3e9109bc320a23c493ec134d1073d6f4ba2e
James <wayperwayp@gmail.com>
2019-06-07 14:45:56 +0800

    added firebase

--- lib/models/AppModel.dart
+++ lib/models/AppModel.dart
@@ -5,49 +5,78 @@ import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 import 'EditReceiptScreenModel.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
-
-enum Period { MONTHLY, WEEKLY, DAILY } 
+enum Period { MONTHLY, WEEKLY, DAILY }
 
 class AppModel extends Model {
-
- 
   User _user;
   Period _viewing_period = Period.DAILY;
   bool _alsoReceivePromosThruEmail;
   bool _receiveUniquePromos;
   bool _receiveOpenToAllPromos;
   List<String> _receipts_json_paths;
+  // final GoogleSignIn googleSignIn = GoogleSignIn();
+  // FirebaseAuth mAuth;
 
-  User get user => _user; 
-  bool get alsoReceivePromosThruEmail => _alsoReceivePromosThruEmail; 
+  User get user => _user;
+  bool get alsoReceivePromosThruEmail => _alsoReceivePromosThruEmail;
   Period get viewing_period => _viewing_period;
   bool get receiveUniquePromos => _receiveUniquePromos;
   bool get receiveOpenToAllPromos => _receiveOpenToAllPromos;
 
-
-  
   String directoryPath;
-  EditReceiptScreenModel editReceiptScreenModel = EditReceiptScreenModel(); 
+  EditReceiptScreenModel editReceiptScreenModel = EditReceiptScreenModel();
   // ChartScreenModel chartScreenModel;
-  
 
-
-  AppModel(){
-    init(); 
+  AppModel() {
+    init();
   }
+
+  void init() { 
+
+    // mAuth = FirebaseAuth.instance;
+ 
+  }
+
+   login(String email , String password)async { 
+
+      //  mAuth.signInWithEmailAndPassword(email: email, password:  password );
+      //  print("Signed in: " + mAuth.currentUser().toString());
+
+
+      // user.fbUser = await _handleSignIn();
+
+  }
+
+
+
+//   Future<FirebaseUser> _handleSignIn() async {
+//   final GoogleSignInAccount googleUser = await googleSignIn.signIn();
+//   final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+//   final AuthCredential credential = GoogleAuthProvider.getCredential(
+//     accessToken: googleAuth.accessToken,
+//     idToken: googleAuth.idToken,
+//   );
+
+//   final FirebaseUser user = await mAuth.signInWithCredential(credential);
+//   print("signed in " + user.displayName);
+//   return user;
+// }
 
   set user(User user) {
     _user = user;
     notifyListeners();
   }
 
-  set receiveUniquePromos(bool value){ 
+  set receiveUniquePromos(bool value) {
     _receiveUniquePromos = value;
     notifyListeners();
   }
 
-  set receiveOpenToAllPromos(bool value){
+  set receiveOpenToAllPromos(bool value) {
     _receiveOpenToAllPromos = value;
     notifyListeners();
   }
@@ -58,38 +87,19 @@ class AppModel extends Model {
   }
 
   set viewing_period(Period period) {
-    _viewing_period = period; 
+    _viewing_period = period;
     notifyListeners();
   }
 
-  
-
-  void init() async {
-    
-    print("The local path is !! : "+_localPath.toString());
-
-    // directoryPath =  
-
-
-
-  }
-
   Future<String> get _localPath async {
-  final directory = await getApplicationDocumentsDirectory();
+    final directory = await getApplicationDocumentsDirectory();
 
-  return directory.path;
-}
-
-
-
-
-
-
+    return directory.path;
+  }
 }
 
 class User {
   String username;
   String uid;
+  // FirebaseUser fbUser;
 }
-
- 
--- lib/screens/LogInScreen.dart
+++ lib/screens/LogInScreen.dart
@@ -4,6 +4,8 @@ import 'package:papyrus_client/helpers/ClipShadowPath.dart';
 import 'package:papyrus_client/helpers/CustomShapeClipper.dart';
 import 'package:papyrus_client/helpers/LongButton.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:papyrus_client/models/AppModel.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LogInScreen extends StatefulWidget {
   @override
@@ -33,149 +35,158 @@ class _LogInScreenStackState extends State<LogInScreenStack> {
   FocusNode pass_focus = FocusNode();
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
+    return ScopedModelDescendant<AppModel>(
+      // stream: null,
+      builder: (context, child, appModel) {
+        return new Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
 
-          // gradient: greeny,
-          color: Colors.green),
-      child: Stack(
-        children: <Widget>[
-          ClipShadowPath(
-              shadow: Shadow(
-                  blurRadius: 10 * sizeMul,
-                  offset: Offset(0, sizeMul),
-                  color: Colors.black38.withAlpha(0)),
-              clipper: CustomShapeClipper(
-                  sizeMul: sizeMul,
-                  maxWidth: MediaQuery.of(context).size.width,
-                  maxHeight: MediaQuery.of(context).size.width * 0.91),
-              child: Container(
-                color: Colors.white,
-                width: MediaQuery.of(context).size.width,
-                height: double.infinity,
-              )),
+              // gradient: greeny,
+              color: Colors.green),
+          child: Stack(
+            children: <Widget>[
+              ClipShadowPath(
+                  shadow: Shadow(
+                      blurRadius: 10 * sizeMul,
+                      offset: Offset(0, sizeMul),
+                      color: Colors.black38.withAlpha(0)),
+                  clipper: CustomShapeClipper(
+                      sizeMul: sizeMul,
+                      maxWidth: MediaQuery.of(context).size.width,
+                      maxHeight: MediaQuery.of(context).size.width * 0.91),
+                  child: Container(
+                    color: Colors.white,
+                    width: MediaQuery.of(context).size.width,
+                    height: double.infinity,
+                  )),
+
+                  Positioned(
+                    child: Text("Log in\nto start saving", style: TextStyle(fontSize: sizeMul*40, fontWeight: FontWeight.w900),
+
+                    ),
+                  ),
+              Positioned(
+                top: sizeMul * 226,
+                right: MediaQuery.of(context).size.width * 0.073,
+                child: Material(
+                  color: Colors.white.withAlpha(0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          CupertinoPageRoute(builder: (context) => HomeScreen()));
+                    },
+                    child: Image.asset(
+                      "assets/icons/3x/papygreen.png",
+                      width: 70 * sizeMul,
+                    ),
+                  ),
+                ),
+              ),
 
               Positioned(
-                child: Text("Log in\nto start saving", style: TextStyle(fontSize: sizeMul*40, fontWeight: FontWeight.w900),
+                bottom: 0,
+                left: 0,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2,
+                  padding: EdgeInsets.all(sizeMul * 20),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: sizeMul * 30,
+                      ),
+                      Container(
+                        width: sizeMul * 260,
+                        height: sizeMul * 50,
+                        padding: EdgeInsets.symmetric(horizontal: sizeMul * 15),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(3000)),
+                            border: Border.all(
+                                color: Colors.white, width: sizeMul * 2)),
+                        child: Center(
+                          child: EditableText(
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                color: Colors.white, fontSize: sizeMul * 23),
+                            backgroundCursorColor: Colors.red,
+                            cursorColor: Colors.pinkAccent,
+                            focusNode: email_focus,
+                            controller: email_controller,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: sizeMul * 30,
+                      ),
+                      Container(
+                        width: sizeMul * 260,
+                        height: sizeMul * 50,
+                        padding: EdgeInsets.symmetric(horizontal: sizeMul * 15),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(3000)),
+                            border: Border.all(
+                                color: Colors.white, width: sizeMul * 2)),
+                        child: Center(
+                          child: EditableText(
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                color: Colors.white, fontSize: sizeMul * 23),
+                            backgroundCursorColor: Colors.red,
+                            cursorColor: Colors.pinkAccent,
+                            focusNode: pass_focus,
+                            controller: pass_controller,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: sizeMul * 30,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.symmetric(vertical: sizeMul * 14),
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(sizeMul * 35))),
+                        child: OutlineButton(
+                          highlightedBorderColor: Colors.white,
+                          highlightColor: Colors.green,
+                          textColor: Colors.white,
+                          disabledBorderColor: Colors.white,
+                          color: Colors.white,
+                          borderSide:
+                              BorderSide(color: Colors.white, width: sizeMul * 2),
+                          child: Text(
+                            "Log in",
+                            style: TextStyle(fontSize: sizeMul * 19),
+                          ),
+                          splashColor: Colors.greenAccent,
+                          highlightElevation: 5,
+                          clipBehavior: Clip.none,
+                          onPressed: () {
 
+                            appModel.login("user@user.com", "useruser");
+
+ 
+                            // Navigator.push(
+                            //     context,
+                            //     CupertinoPageRoute(
+                            //         builder: (context) => HomeScreen()));
+                            // showDialog(
+                            //     context: context,
+                            //     builder: (BuildContext context) {});
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-          Positioned(
-            top: sizeMul * 226,
-            right: MediaQuery.of(context).size.width * 0.073,
-            child: Material(
-              color: Colors.white.withAlpha(0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      CupertinoPageRoute(builder: (context) => HomeScreen()));
-                },
-                child: Image.asset(
-                  "assets/icons/3x/papygreen.png",
-                  width: 70 * sizeMul,
-                ),
-              ),
-            ),
+              )
+            ],
           ),
-
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2,
-              padding: EdgeInsets.all(sizeMul * 20),
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: sizeMul * 30,
-                  ),
-                  Container(
-                    width: sizeMul * 260,
-                    height: sizeMul * 50,
-                    padding: EdgeInsets.symmetric(horizontal: sizeMul * 15),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(3000)),
-                        border: Border.all(
-                            color: Colors.white, width: sizeMul * 2)),
-                    child: Center(
-                      child: EditableText(
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            color: Colors.white, fontSize: sizeMul * 23),
-                        backgroundCursorColor: Colors.red,
-                        cursorColor: Colors.pinkAccent,
-                        focusNode: email_focus,
-                        controller: email_controller,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: sizeMul * 30,
-                  ),
-                  Container(
-                    width: sizeMul * 260,
-                    height: sizeMul * 50,
-                    padding: EdgeInsets.symmetric(horizontal: sizeMul * 15),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(3000)),
-                        border: Border.all(
-                            color: Colors.white, width: sizeMul * 2)),
-                    child: Center(
-                      child: EditableText(
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            color: Colors.white, fontSize: sizeMul * 23),
-                        backgroundCursorColor: Colors.red,
-                        cursorColor: Colors.pinkAccent,
-                        focusNode: pass_focus,
-                        controller: pass_controller,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: sizeMul * 30,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: EdgeInsets.symmetric(vertical: sizeMul * 14),
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.all(Radius.circular(sizeMul * 35))),
-                    child: OutlineButton(
-                      highlightedBorderColor: Colors.white,
-                      highlightColor: Colors.green,
-                      textColor: Colors.white,
-                      disabledBorderColor: Colors.white,
-                      color: Colors.white,
-                      borderSide:
-                          BorderSide(color: Colors.white, width: sizeMul * 2),
-                      child: Text(
-                        "Log in",
-                        style: TextStyle(fontSize: sizeMul * 19),
-                      ),
-                      splashColor: Colors.greenAccent,
-                      highlightElevation: 5,
-                      clipBehavior: Clip.none,
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            CupertinoPageRoute(
-                                builder: (context) => HomeScreen()));
-                        // showDialog(
-                        //     context: context,
-                        //     builder: (BuildContext context) {});
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+        );
+      }
     );
   }
 }
