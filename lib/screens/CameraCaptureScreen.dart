48ff1fa7ebb130248a12094f9f68454946dd4599
James <wayperwayp@gmail.com>
2019-06-08 14:19:09 +0800

    good camera

--- lib/screens/CameraCaptureScreen.dart
+++ lib/screens/CameraCaptureScreen.dart
@@ -6,6 +6,7 @@ import 'HomeScreen.dart';
 class CameraScreenState {
   List<CameraDescription> cameras;
   CameraController controller;
+  bool controllerInitSuccesful = false;
 }
 
 class CameraCaptureScreen extends StatefulWidget {
@@ -13,18 +14,33 @@ class CameraCaptureScreen extends StatefulWidget {
   _CameraAppState createState() => _CameraAppState();
 }
 
-initializeCameras(CameraScreenState cs, _CameraAppState c) async {
-  cs.cameras = await availableCameras();
-  print(cs.cameras.map((cam) => ("The camera is: ${cam.name}")));
+// Future<>initializeCameras(CameraScreenState cs, _CameraAppState c) async {
+//   cs.cameras = await availableCameras();
+//   print(cs.cameras.map((cam) => ("The camera is: ${cam.name}")));
 
+//   cs.controller = CameraController(
+//       cs.cameras[cs.cameras.length - 1], ResolutionPreset.medium);
+//   cs.controller.initialize().then((_) {
+//     // if (!mounted) {
+//     //   return;
+//     // }
+//     c.setState(() {});
+//   });
+// }
+
+Future<CameraController> getController(CameraScreenState cs,_CameraAppState c) async {
+  cs.cameras = await availableCameras();
   cs.controller = CameraController(
       cs.cameras[cs.cameras.length - 1], ResolutionPreset.medium);
-  cs.controller.initialize().then((_) {
-    // if (!mounted) {
-    //   return;
-    // }
+  try {
+    await cs.controller.initialize();
+    cs.controllerInitSuccesful = true;
+    return cs.controller;
+  } catch (a) {
+    cs.controllerInitSuccesful = false;
+    return cs.controller;
+  }
     c.setState(() {});
-  });
 }
 
 class _CameraAppState extends State<CameraCaptureScreen> {
@@ -34,7 +50,7 @@ class _CameraAppState extends State<CameraCaptureScreen> {
   @override
   void initState() {
     super.initState();
-    initializeCameras(cs, this);
+    // initializeCameras(cs, this);
   }
 
   @override
@@ -45,53 +61,62 @@ class _CameraAppState extends State<CameraCaptureScreen> {
 
   @override
   Widget build(BuildContext context) {
-    if (!cs.controller.value.isInitialized) {
-      return Container(width: MediaQuery.of(context).size.width,height: 1,);
-    }
-    return AspectRatio(
-        aspectRatio: cs.controller.value.aspectRatio,
-        child: Stack(
-          children: <Widget>[
-            CameraPreview(cs.controller),
-            Positioned(
-              left: sizeMul * 1,
-              top: sizeMul * 24,
-              child: Material(
-                color: Colors.white.withAlpha(0),
-                child: InkWell(
-                  splashColor: Colors.white.withAlpha(0),
-                  highlightColor: Colors.black.withOpacity(0.1),
-                  // ,
-                  onTap: () {
-                    // editReceiptScreenModel = EditReceiptScreenModel();
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                    width: sizeMul * 40,
-                    padding: EdgeInsets.symmetric(vertical: 10 * sizeMul),
-                    // height: sizeMul*40,
-                    // color: Colors.red,
-                    child: Image.asset(
-                      'assets/icons/3x/back.png',
-                      height: MediaQuery.of(context).size.width * 0.075,
+    // if (!cs.controller.value.isInitialized) {
+    //   return Container(width: MediaQuery.of(context).size.width,height: 1,);
+    // }
+    return FutureBuilder(
+        future: getController(cs,this),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done)
+
+            return (cs.controllerInitSuccesful)?AspectRatio(
+                aspectRatio: snapshot.data.value.aspectRatio,
+                child: Stack(
+                  children: <Widget>[
+                    CameraPreview(snapshot.data),
+                    Positioned(
+                      left: sizeMul * 1,
+                      top: sizeMul * 24,
+                      child: Material(
+                        color: Colors.white.withAlpha(0),
+                        child: InkWell(
+                          splashColor: Colors.white.withAlpha(0),
+                          highlightColor: Colors.black.withOpacity(0.1),
+                          // ,
+                          onTap: () {
+                            // editReceiptScreenModel = EditReceiptScreenModel();
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            width: sizeMul * 40,
+                            padding:
+                                EdgeInsets.symmetric(vertical: 10 * sizeMul),
+                            // height: sizeMul*40,
+                            // color: Colors.red,
+                            child: Image.asset(
+                              'assets/icons/3x/back.png',
+                              height: MediaQuery.of(context).size.width * 0.075,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: sizeMul*60,
-                color: Colors.black.withOpacity(0.3),
-                child: RaisedButton(
-                  onPressed: () {},
-                  shape: CircleBorder(),
-                ),
-              ),
-            )
-          ],
-        ));
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: sizeMul * 60,
+                        color: Colors.black.withOpacity(0.3),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          shape: CircleBorder(),
+                        ),
+                      ),
+                    )
+                  ],
+                )):Center(child: Text("Getting camera failed. Did you reject permission?", style: TextStyle(fontSize: sizeMul*19, color: Colors.black),),);
+          else
+            return Center(child: CircularProgressIndicator());
+        });
   }
 }
